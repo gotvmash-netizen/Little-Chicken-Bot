bb103bda9562a3cd059670df5782f4ee3affd326
gotvmash-netizen <gotvmash@gmail.com>
2026-08-23 20:41:45 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  name: 'ping',
+  execute(message, client, args) {
+    const sent = Date.now();
+    message.reply('Pong! 🏓').then((reply) => {
+      const roundtrip = Date.now() - sent;
+      const wsLatency = client.ws.ping;
+      reply.edit(
+        `Pong! 🏓\n📡 Bot latency: ${roundtrip}ms\n💓 WebSocket latency: ${wsLatency}ms`
+      );
+    });
+  },
+};
