f1c75c27da964fefcaae4399368dafc4bbec5d07
gotvmash-netizen <gotvmash@gmail.com>
2026-09-08 17:08:10 -0300

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const { logCommand } = require('../utils/commandLogger');
 
-const prefix = 'g!';
+const prefix = '!';
 
 // nwtaken i love you
 const prefixCommands = new Map();
